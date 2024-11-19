118615b45bce22fc0dab0ed032ae79a93900dd65
kcooperhn <134466354+kcooperhn@users.noreply.github.com>
2024-11-18 20:20:26 -0600

    escaneo de imagenes desde la camara
    escaneo de texto desde la camara

--- ReconocimientoOCR/app/src/main/java/hn/uth/reconocimientoocr/ui/camera/CameraFragment.java
+++ ReconocimientoOCR/app/src/main/java/hn/uth/reconocimientoocr/ui/camera/CameraFragment.java
@@ -1,19 +1,59 @@
 package hn.uth.reconocimientoocr.ui.camera;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import android.Manifest;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import hn.uth.reconocimientoocr.databinding.FragmentCameraBinding;
 
 public class CameraFragment extends Fragment {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_PERMISSIONS = 100;
 
     private FragmentCameraBinding binding;
+    private Button btnAbrirCamara;
+    private ImageView imgVistaPrevia;
+    private Bitmap imagenSeleccionada;
+    private TextView txtResult;
+    private String directorioImagen;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,10 +62,139 @@ public class CameraFragment extends Fragment {
 
         binding = FragmentCameraBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        directorioImagen = "";
 
-        //final TextView textView = binding.txtResultCamera;
-        //dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        imgVistaPrevia = binding.imgCamera;
+        btnAbrirCamara = binding.btnCamara;
+        txtResult = binding.txtResultCamera;
+
+
+        Log.d("IMAGEN_CAMARA","Pantalla construida");
+        btnAbrirCamara.setOnClickListener(v -> {
+            if(checkAndRequestPermissions()){
+                Log.d("IMAGEN_CAMARA","Permisos aceptados");
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(this.getActivity().getPackageManager()) != null) {
+                    Log.d("IMAGEN_CAMARA","Intent camara resuelto");
+                    File archivoImagen = null;
+                    try{
+                        archivoImagen = createImageFile();
+                    }catch(Exception error){
+                        error.printStackTrace();
+                        Log.d("IMAGEN_CAMARA","Error al generar archivo de imagen");
+                    }
+                    if(archivoImagen != null){
+                        Uri fotoUri = FileProvider.getUriForFile(this.getContext(), "hn.uth.reconocimientoocr.fileprovider", archivoImagen);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fotoUri);
+                    }
+
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    Log.d("IMAGEN_CAMARA","Activity camara iniciada");
+                }else{
+                    Log.d("IMAGEN_CAMARA","No se encontró app para manejo de camara");
+                }
+            }
+        });
         return root;
+    }
+
+    private File createImageFile() throws IOException {
+        String fechaHoy = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String nombreArchivo = "JPEG_"+fechaHoy+"_";
+        File directorio = this.getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imagen = File.createTempFile(nombreArchivo, ".jpg", directorio);
+        directorioImagen = imagen.getAbsolutePath();
+
+        return imagen;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        Log.d("IMAGEN_CAMARA","Data de camara obtenida");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Log.d("IMAGEN_CAMARA","Data contiene imagen");
+
+            if(!"".equals(directorioImagen) && directorioImagen != null){
+                File imgFile = new File(directorioImagen);
+                if(imgFile.exists()){
+                    Bitmap imgAltaResolucion = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                    imagenSeleccionada = imgAltaResolucion;
+                }else{
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    imagenSeleccionada = imageBitmap;
+                }
+            }else{
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                imagenSeleccionada = imageBitmap;
+            }
+            imgVistaPrevia.setImageBitmap(imagenSeleccionada);
+            Log.d("IMAGEN_CAMARA","imagen mostrada en pantalla");
+            ejecutarReconocedorOCR();
+            Log.d("IMAGEN_CAMARA","Reconocedor OCR ejecutado");
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private boolean checkAndRequestPermissions() {
+        int cameraPermission = ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.CAMERA);
+        Log.d("IMAGEN_CAMARA","Evaluando permisos");
+
+        if (android.os.Build.VERSION.SDK_INT <= Build.VERSION_CODES.P){
+            int storagePermission = ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (cameraPermission != PackageManager.PERMISSION_GRANTED || storagePermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this.getActivity(),
+                        new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_PERMISSIONS);
+                Log.d("IMAGEN_CAMARA","Permiso rechazado");
+                return false;
+            }
+        }else{
+            if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this.getActivity(),
+                        new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_PERMISSIONS);
+                Log.d("IMAGEN_CAMARA","Permiso rechazado");
+                return false;
+            }
+        }
+        Log.d("IMAGEN_CAMARA","Permiso concedido");
+        return true;
+    }
+
+    private void ejecutarReconocedorOCR(){
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        InputImage imagenEnPantalla = InputImage.fromBitmap(imagenSeleccionada, 0);
+        recognizer.process(imagenEnPantalla)
+                .addOnSuccessListener(this::processTextRecognitionResult)
+                .addOnFailureListener(e -> {
+                    Log.d("IMAGEN_CAMARA","Error al extraer texto de imagen mediante OCR");
+                    e.printStackTrace();
+                });
+
+    }
+
+    private void processTextRecognitionResult(Text text) {
+        List<Text.TextBlock> bloques = text.getTextBlocks();
+        if(bloques.size() == 0){
+            showToast("No se encontró texto en la imagen");
+            return;
+        }
+        StringBuilder textos = new StringBuilder();
+        for (int i=0; i<bloques.size(); i++){
+            List<Text.Line> lineas = bloques.get(i).getLines();
+
+            for(Text.Line linea: lineas){
+                textos.append(linea.getText()).append("\n");
+            }
+        }
+        txtResult.setText(textos.toString());
+    }
+
+    private void showToast(String mensaje) {
+        Toast.makeText(this.getContext(), mensaje, Toast.LENGTH_LONG).show();
     }
 
     @Override
--- ReconocimientoOCR/app/src/main/java/hn/uth/reconocimientoocr/ui/gallery/GalleryFragment.java
+++ ReconocimientoOCR/app/src/main/java/hn/uth/reconocimientoocr/ui/gallery/GalleryFragment.java
@@ -1,9 +1,11 @@
 package hn.uth.reconocimientoocr.ui.gallery;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Pair;
@@ -11,15 +13,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.chip.Chip;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
@@ -34,14 +39,20 @@ import hn.uth.reconocimientoocr.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
 
+    private static int PICK_IMAGE = 1;
+
     private FragmentGalleryBinding binding;
     private ImageView galleryImage;
     private Button btnSeleccionarImagen;
     private Button btnProcesar;
     private TextView txtResult;
+    private CheckBox reconocimientoDNI;
     private Bitmap imagenSeleccionada;
     private Integer maxImageWidth;
     private Integer maxImageHeight;
+    private int contadorNombre = 0;
+    private int contadorApellido = 0;
+    private String nombre = "";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -51,18 +62,53 @@ public class GalleryFragment extends Fragment {
         View root = binding.getRoot();
         galleryImage = binding.imgGaleria; //ASOCIO EL CONTROL DEL LAYOUT A LA VARIABLE DEL ACTIVITY O FRAGMENT
         btnSeleccionarImagen = binding.btnSeleccionarImagen; //ASOCIO EL CONTROL DEL LAYOUT A LA VARIABLE DEL ACTIVITY O FRAGMENT
-        View.OnClickListener btnClick = v -> mostrarImagen();
+        View.OnClickListener btnClick = v -> seleccionarImagenGaleria();
         btnSeleccionarImagen.setOnClickListener(btnClick);
 
         btnProcesar = binding.btnProcesar;//ASOCIO EL CONTROL DEL LAYOUT A LA VARIABLE DEL ACTIVITY O FRAGMENT
         btnProcesar.setOnClickListener(v -> ejecutarReconocedorOCR());
 
         txtResult = binding.txtResult;//ASOCIO EL CONTROL DEL LAYOUT A LA VARIABLE DEL ACTIVITY O FRAGMENT
+        reconocimientoDNI = binding.cbDNI;
 
         Log.d("IMAGEN_GALERIA","Control imagen creado");
-        imagenSeleccionada = obtenerImagenDesdeAsset(this.getContext(), "identidad.jpg");
+        //imagenSeleccionada = obtenerImagenDesdeAsset(this.getContext(), "identidad.jpg");
 
         return root;
+    }
+
+    private void seleccionarImagenGaleria(){
+        Intent galeriaIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galeriaIntent.setType("image/*");
+
+        Intent seleccionIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        seleccionIntent.setType("image/*");
+
+        Intent menuIntent = Intent.createChooser(galeriaIntent, "Seleccione una Imagen");
+        menuIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{seleccionIntent});
+
+        startActivityForResult(menuIntent, PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == PICK_IMAGE){
+            if(data == null){
+                showToast("Favor seleccionar una foto de la galeria");
+            }else{
+                final Uri uri = data.getData();
+                InputImage image;
+                try{
+                    image = InputImage.fromFilePath(this.getContext(), uri);
+                    imagenSeleccionada = image.getBitmapInternal();
+                    mostrarImagen();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void mostrarImagen(){
@@ -136,17 +182,43 @@ public class GalleryFragment extends Fragment {
             return;
         }
         StringBuilder textos = new StringBuilder();
+        boolean reconocerDNI=reconocimientoDNI.isChecked();
         for (int i=0; i<bloques.size(); i++){
             List<Text.Line> lineas = bloques.get(i).getLines();
+
             textos.append(lineas.get(0).getText()).append("\n");
-            for(int l=0; l<lineas.size();l++){
-                List<Text.Element> elementos = lineas.get(l).getElements();
-                for(int e=0;e<elementos.size(); e++){
-                    Log.d("IMAGEN_GALERIA","ELEMENT -> "+elementos.get(e));
-                }
+            if(reconocerDNI){
+                procesarDNI(lineas);
             }
         }
         txtResult.setText(textos.toString());
+    }
+
+    private void procesarDNI(List<Text.Line> lineas){
+        for(int l=0; l<lineas.size();l++){
+            boolean comienzaNombre = validarNombre(lineas.get(l).getText());
+            if(contadorApellido > 0 && !"".equals(nombre)){
+                nombre = nombre + " " + lineas.get(l).getText() ;
+                Log.d("IMAGEN_GALERIA","APELLIDO -> "+nombre);
+                showToast("Nombre Completo: " + nombre);
+                contadorApellido = 0;
+            }
+            if(contadorNombre > 0){
+                nombre = lineas.get(l).getText() ;
+                Log.d("IMAGEN_GALERIA","NOMBRE -> "+nombre);
+                contadorNombre = 0;
+                contadorApellido++;
+            }
+            if(comienzaNombre && contadorNombre == 0){
+                Log.d("IMAGEN_GALERIA","VALIDA NOMBRE -> "+lineas.get(l).getText());
+                contadorNombre++;
+            }
+        }
+    }
+
+    private boolean validarNombre(String valor) {
+        Log.d("IMAGEN_GALERIA","VALIDANDO NOMBRE -> "+valor);
+        return valor.startsWith("NACIONAL DE IDENTIFICA") || valor.contains("NACIONAL DE IDENTIFICA");
     }
 
     private void showToast(String mensaje) {
